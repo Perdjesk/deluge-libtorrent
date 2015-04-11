9de64f514ea53e96fabccf794bf93b01c298c57e
jon <jon@users.noreply.github.com>
2015-04-11 20:42:32 +0200

    comments, port udp

--- latest/command.sh
+++ latest/command.sh
@@ -4,9 +4,9 @@ deluged -c /config -L info -l /config/deluged.log
 
 if [ ! -f /config/core.conf ]; then
     sleep 2
-    echo "Creating default configuration and adding default /data path"
+    echo "Pushing configuration for core.conf (deluge)"
     deluge-console -c /config "config -s move_completed_path /data/completed"
-    deluge-console -c /config "config -s torrentfiles_location /data/torrents"
+    deluge-console -c /config "config -s torrentfiles_location /data/torrentsfiles"
     deluge-console -c /config "config -s download_location /data/download"
     deluge-console -c /config "config -s autoadd_location /data/autoadd"
 	deluge-console -c /config "config -s random_port false"

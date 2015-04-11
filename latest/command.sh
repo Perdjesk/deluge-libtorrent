7424f30e8058d3a680c2c8ba19e5b9042ac07799
jon <jon@users.noreply.github.com>
2015-04-11 19:06:39 +0200

    ports

--- latest/command.sh
+++ latest/command.sh
@@ -9,6 +9,10 @@ if [ ! -f /config/core.conf ]; then
     deluge-console -c /config "config -s torrentfiles_location /data/torrents"
     deluge-console -c /config "config -s download_location /data/download"
     deluge-console -c /config "config -s autoadd_location /data/autoadd"
+	deluge-console -c /config "config -s random_port false"
+	deluge-console -c /config "config -s listen_ports (6881, 6881)"
+  ],
+
 fi
 
 deluge-web -c /config

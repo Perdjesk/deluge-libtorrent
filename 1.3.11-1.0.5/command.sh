7088a642d0449a39faa919a852caa2bb8b452a46
jon <jon@localhost.localdomain>
2015-05-16 21:54:45 +0200

    supervisor services

--- 1.3.11-1.0.5/command.sh
+++ 1.3.11-1.0.5/command.sh
@@ -1,18 +1,20 @@
 #!/bin/sh
 
-deluged -c /config -L info -l /config/deluged.log
+chown deluge:deluge /config
+chown deluge:deluge /data
 
 if [ ! -f /config/core.conf ]; then
+    supervisord -c /etc/supervisor/supervisord.conf
     sleep 2
     echo "Pushing configuration for core.conf (deluge)"
     deluge-console -c /config "config -s move_completed_path /data/completed"
     deluge-console -c /config "config -s torrentfiles_location /data/torrentsfiles"
     deluge-console -c /config "config -s download_location /data/download"
     deluge-console -c /config "config -s autoadd_location /data/autoadd"
-	deluge-console -c /config "config -s random_port false"
-	deluge-console -c /config "config -s listen_ports (6881, 6881)"
-  ],
-
+    deluge-console -c /config "config -s random_port false"
+    deluge-console -c /config "config -s listen_ports (6881, 6881)"
+    pkill supervisord
+    sleep 2
 fi
 
-deluge-web -c /config
+supervisord -c /etc/supervisor/supervisord.conf -n
